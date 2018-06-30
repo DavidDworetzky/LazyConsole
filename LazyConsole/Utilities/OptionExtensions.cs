c2b5eeb5e1e9eb23531053ac033656f5caf5f931
David Dworetzky <djdworetz@gmail.com>
2018-06-30 16:00:15 -0500

    Updated package version and changed accessibiility on another internal class

--- LazyConsole/Utilities/OptionExtensions.cs
+++ LazyConsole/Utilities/OptionExtensions.cs
@@ -4,9 +4,9 @@ using System.Text;
 
 namespace LazyConsole.Utilities
 {
-    public static class OptionExtensions
+    internal static class OptionExtensions
     {
-        public static int CompareOptions(KeyValuePair<char, Tuple<string, Action>> optionA, KeyValuePair<char, Tuple<string, Action>> optionB)
+        internal static int CompareOptions(KeyValuePair<char, Tuple<string, Action>> optionA, KeyValuePair<char, Tuple<string, Action>> optionB)
         {
             string optionAValue = new string(new char[] { optionA.Key });
             string optionBValue = new string(new char[] { optionB.Key });
